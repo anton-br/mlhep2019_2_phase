e48df4f0654b97611fee4d0dac9d16d7c1b77787
anton-br <ziliboba753@gmail.com>
2019-07-09 21:37:28 +0000

    update generator

--- analysis/generator.py
+++ analysis/generator.py
@@ -8,9 +8,12 @@ class ModelGConvTranspose(nn.Module):
         self.z_dim = z_dim
         super(ModelGConvTranspose, self).__init__()
         self.fc1 = nn.Linear(self.z_dim + 2 + 3, 256*4*4)
-
+#         self.fc2 = nn.Linear(64, 128)
+#         self.fc3 = nn.Linear(128, 512)
+#         self.fc4 = nn.Linear(512, 20736)
+        
         self.conv1 = nn.Conv2d(256, 128, 3, padding=1)
-        self.conv1_2 = nn.Conv2d(128, 128, 3, padding=1)
+#         self.conv1_2 = nn.Conv2d(128, 128, 3, padding=1)
 
         self.dropout = nn.Dropout(p=0.1)
         self.up = nn.Upsample(scale_factor=2)
@@ -19,8 +22,8 @@ class ModelGConvTranspose(nn.Module):
         self.conv2 = nn.Conv2d(128, 64, 3, padding=1)
         self.conv2_2 = nn.Conv2d(64, 64, 3, padding=1)
 
-        self.conv3 = nn.Conv2d(64, 64, 3, padding=1)
-        self.conv3_2 = nn.Conv2d(64, 64, 3, padding=1)
+#         self.conv3 = nn.Conv2d(64, 64, 3, padding=1)
+#         self.conv3_2 = nn.Conv2d(64, 64, 3, padding=1)
 
         self.conv4 = nn.Conv2d(64, 32, 3, padding=1)
         self.conv4_2 = nn.Conv2d(32, 32, 3, padding=1)
@@ -31,22 +34,21 @@ class ModelGConvTranspose(nn.Module):
         self.conv6 = nn.Conv2d(16, 8, 3, padding=1)
         self.conv7 = nn.Conv2d(8, 1, 3)
         
-        
     def forward(self, z, ParticleMomentum_ParticlePoint):
         x = F.leaky_relu(self.fc1(
             torch.cat([z, ParticleMomentum_ParticlePoint], dim=1)
         ))
         EnergyDeposit = x.view(-1, 256, 4, 4)
-        EnergyDeposit = F.leaky_relu(self.conv1(EnergyDeposit))
-        EnergyDeposit = self.up(self.dropout(F.leaky_relu(self.conv1_2(EnergyDeposit))))
+        EnergyDeposit = self.up(F.leaky_relu(self.conv1(EnergyDeposit)))
+#         EnergyDeposit = self.up(self.dropout(F.leaky_relu(self.conv1_2(EnergyDeposit))))
         
         EnergyDeposit = F.leaky_relu(self.conv2(EnergyDeposit))
         EnergyDeposit = self.up(self.dropout(F.leaky_relu(self.conv2_2(EnergyDeposit))))
 
-        EnergyDeposit = F.leaky_relu(self.conv3(EnergyDeposit))
-        EnergyDeposit = self.up(self.dropout(F.leaky_relu(self.conv3_2(EnergyDeposit))))
+#         EnergyDeposit = F.leaky_relu(self.conv3(EnergyDeposit))
+#         EnergyDeposit = self.up(self.dropout(F.leaky_relu(self.conv3_2(EnergyDeposit))))
         
-        EnergyDeposit = F.leaky_relu(self.conv4(EnergyDeposit))
+        EnergyDeposit = self.up(F.leaky_relu(self.conv4(EnergyDeposit)))
         EnergyDeposit = self.up(self.dropout(F.leaky_relu(self.conv4_2(EnergyDeposit))))
         
         EnergyDeposit = F.leaky_relu(self.conv5(EnergyDeposit))
